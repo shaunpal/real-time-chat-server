be01b08dcb731421a21d3e23eaa4db23d4557684
shaun <shaun@shauns-MacBook-Air.local>
2023-11-30 21:28:01 +0800

    Updated configs

--- router.js
+++ router.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/api", (req, res) => {
     res.send('Server is running');
+    res.end('Hello World!');
 })
 
 module.exports = router;
